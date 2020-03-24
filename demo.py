44f2eff82478b905eab5ed48870016aa6737d70e
waacd <waacd@waacd.localdomain>
2020-03-24 21:52:11 +0800

    second

--- demo.py
+++ demo.py
@@ -7,3 +7,15 @@ def close(window):
     window.destroy()
     return None
 #函数：添加姓名窗口，参数，无，返回值
+def add():
+    #添加成绩窗口
+    stuname = simpledialog.askstring('添加姓名','请输入学生姓名')
+    if stuname in dict_stu.keys():
+        showinfo(title = '错误',message='该学生已存在！')
+    else:
+        #添加成绩
+        score = simpledialog.askstring('添加成绩','请输入学生成绩')
+        #写入字典
+        dict_stu[stuname] = score
+        showinfo('提示','添加成功！')
+    return None
